3795b28cbac4fb1939edfe92eddaf95710a6ceda
MicroLin <qihuanlu@126.com>
2018-09-05 07:45:06 +0800

    up

--- ConsoleApp2/MyApp.cs
+++ ConsoleApp2/MyApp.cs
@@ -11,20 +11,20 @@ namespace ConsoleApp2
     class MyApp
     {
 
-        const string Format = @"^\d\d[:]?\d\d[:]?\d\d";
-        const string Format2 = @"^\d\d[:]?\d\d[:]?\d\d-->\d\d[:]?\d\d[:]?\d\d";
-        const string Format3 = @"^\d\d[:]?\d\d[:]?\d\d[.]\d{3}-->\d\d[:]?\d\d[:]?\d\d[.]\d{3}";
+        const string Format = @"^\d\d[:]?\d\d[:]?\d\d$";
+        const string Format2 = @"^\d\d[:]?\d\d[:]?\d\d-->\d\d[:]?\d\d[:]?\d\d$";
+        const string Format3 = @"^\d\d[:]?\d\d[:]?\d\d[.]\d{3}-->\d\d[:]?\d\d[:]?\d\d[.]\d{3}$";
         const string Format4 = @"^\D.+";
         //const string Format5 = @"^[0-9]*$";
 
+        const string path = @"C:\Users\Administrator\source\repos\ConsoleApp2\ConsoleApp2\ad.srt";
 
-        const string path = @"C:\Users\Administrator\source\repos\ConsoleApp2\ConsoleApp2\";
 
         TimeStamp timeStamp = new TimeStamp();
 
         bool invalid = false;
         public int counter { get; set; } = 0;
-        public string Line { get; set; }
+      // public string Line { get; set; }
 
         public void run()
         {
@@ -38,16 +38,24 @@ namespace ConsoleApp2
 
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (Line == Environment.NewLine)
-                    {
 
+                   // Console.WriteLine(line);
+                    //Console.ReadKey();
+
+                    if (line == Environment.NewLine)
+                    {
+                        continue;
                     }
                     else if (IsValidTimeFormat(line, Format))
                     {
                         timeStamp.First = line;
 
                         timeStamp.ChangeStingFormat(ref timeStamp.First);
-                        
+
+                        Console.WriteLine(line);
+
+                        Console.ReadKey();
+
                     }
                     else if (IsValidTimeFormat(line, Format2))
                     {
@@ -64,16 +72,21 @@ namespace ConsoleApp2
                         timeStamp.ChangeStingFormat(ref timeStamp.First);
                         timeStamp.Last = vs[1];
                         timeStamp.ChangeStingFormat(ref timeStamp.Last);
-                        timeStamp.TimeSpanSplict();
-                    }
+                        text = timeStamp.TimeSpanSplict();
+                        Console.WriteLine(text);
+                        Console.ReadKey();
 
-                    else if (IsValidTimeFormat(Line, Format3))
+                    }
+                    else if (IsValidTimeFormat(line, Format3))
                     {
-
+                        continue;
                     }
-                    else if (IsValidTimeFormat(Line, Format4))
+
+                    else if (IsValidTimeFormat(line, Format4))
                     {
                         Subtitle = line;
+                        Console.WriteLine(Subtitle);
+                        Console.ReadKey();
                     }
 
                     text = text + line;
@@ -91,8 +104,6 @@ namespace ConsoleApp2
 
         public bool IsValidTimeFormat(string strIn, string Format)
         {
-
-
             invalid = false;
             if (String.IsNullOrEmpty(strIn))
                 return false;
@@ -104,11 +115,13 @@ namespace ConsoleApp2
             {
                 return Regex.IsMatch(strIn,
                                       Format,
-                                      RegexOptions.IgnoreCase,
+                                      RegexOptions.None,
                                       TimeSpan.FromMilliseconds(250));
             }
-            catch (RegexMatchTimeoutException)
+            catch (RegexMatchTimeoutException e)
             {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
                 return false;
             }
         }
